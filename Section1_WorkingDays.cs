7cd345e4eeb38877248117e6fdbd272532bec2f8
Hansi_98 <hansipabasara1@gmail.com>
2020-08-04 21:25:52 +0530

    working days

---
+++ Section1_WorkingDays.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ABC_Institute___Timetable_Generator
+{
+    public partial class Section1_WorkingDays : Form
+    {
+        public Section1_WorkingDays()
+        {
+            InitializeComponent();
+        }
+    }
+}
