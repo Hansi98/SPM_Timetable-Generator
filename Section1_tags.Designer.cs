df31fe54a65424b5633fb751f05d3f236556db8b
chamikaVisal <chamikaperera456@gmail.com>
2020-08-11 14:44:48 +0530

    visal_spring_1 interfaces completed

--- Section1_tags.Designer.cs
+++ Section1_tags.Designer.cs
@@ -30,27 +30,27 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Section1_tags));
             this.panel1 = new System.Windows.Forms.Panel();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.vdataGridTags = new System.Windows.Forms.DataGridView();
+            this.vbtnUpdateTag = new System.Windows.Forms.Button();
+            this.vbtnAddTag = new System.Windows.Forms.Button();
+            this.vtxtBoxTagName = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.serviceController1 = new System.ServiceProcess.ServiceController();
             this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.vdataGridTags)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.panel1.Controls.Add(this.dataGridView1);
-            this.panel1.Controls.Add(this.button3);
-            this.panel1.Controls.Add(this.button2);
-            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.vdataGridTags);
+            this.panel1.Controls.Add(this.vbtnUpdateTag);
+            this.panel1.Controls.Add(this.vbtnAddTag);
+            this.panel1.Controls.Add(this.vtxtBoxTagName);
             this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.button1);
             this.panel1.Controls.Add(this.label1);
@@ -62,56 +62,56 @@
             this.panel1.TabIndex = 0;
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
-            // dataGridView1
+            // vdataGridTags
             // 
-            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.vdataGridTags.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(22, 207);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(919, 284);
-            this.dataGridView1.TabIndex = 15;
+            this.vdataGridTags.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.vdataGridTags.Location = new System.Drawing.Point(22, 207);
+            this.vdataGridTags.Name = "vdataGridTags";
+            this.vdataGridTags.RowHeadersWidth = 51;
+            this.vdataGridTags.RowTemplate.Height = 24;
+            this.vdataGridTags.Size = new System.Drawing.Size(919, 284);
+            this.vdataGridTags.TabIndex = 15;
             // 
-            // button3
+            // vbtnUpdateTag
             // 
-            this.button3.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.button3.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(510, 144);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(226, 44);
-            this.button3.TabIndex = 14;
-            this.button3.Text = "Update Details";
-            this.button3.UseVisualStyleBackColor = false;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.vbtnUpdateTag.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.vbtnUpdateTag.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.vbtnUpdateTag.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.vbtnUpdateTag.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.vbtnUpdateTag.Location = new System.Drawing.Point(510, 144);
+            this.vbtnUpdateTag.Name = "vbtnUpdateTag";
+            this.vbtnUpdateTag.Size = new System.Drawing.Size(226, 44);
+            this.vbtnUpdateTag.TabIndex = 14;
+            this.vbtnUpdateTag.Text = "Update Details";
+            this.vbtnUpdateTag.UseVisualStyleBackColor = false;
+            this.vbtnUpdateTag.Click += new System.EventHandler(this.button3_Click);
             // 
-            // button2
+            // vbtnAddTag
             // 
-            this.button2.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(278, 144);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(226, 44);
-            this.button2.TabIndex = 13;
-            this.button2.Text = "Add details";
-            this.button2.UseVisualStyleBackColor = false;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.vbtnAddTag.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.vbtnAddTag.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.vbtnAddTag.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.vbtnAddTag.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.vbtnAddTag.Location = new System.Drawing.Point(278, 144);
+            this.vbtnAddTag.Name = "vbtnAddTag";
+            this.vbtnAddTag.Size = new System.Drawing.Size(226, 44);
+            this.vbtnAddTag.TabIndex = 13;
+            this.vbtnAddTag.Text = "Add details";
+            this.vbtnAddTag.UseVisualStyleBackColor = false;
+            this.vbtnAddTag.Click += new System.EventHandler(this.button2_Click);
             // 
-            // textBox1
+            // vtxtBoxTagName
             // 
-            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            this.vtxtBoxTagName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox1.Location = new System.Drawing.Point(346, 101);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(374, 22);
-            this.textBox1.TabIndex = 4;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.vtxtBoxTagName.Location = new System.Drawing.Point(346, 101);
+            this.vtxtBoxTagName.Name = "vtxtBoxTagName";
+            this.vtxtBoxTagName.Size = new System.Drawing.Size(374, 22);
+            this.vtxtBoxTagName.TabIndex = 4;
+            this.vtxtBoxTagName.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // label2
             // 
@@ -141,7 +141,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(79, 28);
+            this.label1.Location = new System.Drawing.Point(129, 29);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(72, 29);
             this.label1.TabIndex = 1;
@@ -152,7 +152,7 @@
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.Location = new System.Drawing.Point(22, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(37, 50);
+            this.pictureBox1.Size = new System.Drawing.Size(91, 85);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -168,7 +168,7 @@
             this.Text = "Section1_tags";
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.vdataGridTags)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
@@ -181,10 +181,10 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox vtxtBoxTagName;
+        private System.Windows.Forms.Button vbtnUpdateTag;
+        private System.Windows.Forms.Button vbtnAddTag;
+        private System.Windows.Forms.DataGridView vdataGridTags;
         private System.ServiceProcess.ServiceController serviceController1;
     }
 }
