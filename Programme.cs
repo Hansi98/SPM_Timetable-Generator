f4387b12d7470e89acfe58aa5676fcd6bba5a530
chamikaVisal <chamikaperera456@gmail.com>
2020-08-13 16:50:25 +0530

    Students section party completed

---
+++ Programme.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABC_Institute___Timetable_Generator
+{
+    class Programme
+    {
+        public int proID { get; set; }
+
+        public string proName { get; set; }
+
+        public string proDescription { get; set; }
+    }
+
+}
