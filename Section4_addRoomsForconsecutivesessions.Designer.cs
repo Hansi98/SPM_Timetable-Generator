b23e75f75ab9a79a5890ea29696d1a0deb83239a
Nishiki97 <nsihikiyapa1997@gmail.com>
2020-09-27 02:15:05 +0530

    Section4 - add room for sessions and consecutive sessions - completed

--- Section4_addRoomsForconsecutivesessions.Designer.cs
+++ Section4_addRoomsForconsecutivesessions.Designer.cs
@@ -29,10 +29,17 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Section4_addRoomsForconsecutivesessions));
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.nishikistatimg = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.dgvnishikiconsecsessionsrooms = new System.Windows.Forms.DataGridView();
+            this.btnnishikiaddlecturerrooms = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.nishikistatimg)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvnishikiconsecsessionsrooms)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
@@ -54,9 +61,9 @@
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(95, 33);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(532, 29);
+            this.label1.Size = new System.Drawing.Size(428, 29);
             this.label1.TabIndex = 13;
-            this.label1.Text = "Add Room / Rooms for consecutive sessions";
+            this.label1.Text = "Add Room for consecutive sessions";
             // 
             // nishikistatimg
             // 
@@ -70,12 +77,75 @@
             this.nishikistatimg.TabIndex = 12;
             this.nishikistatimg.TabStop = false;
             // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(997, 170);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 24);
+            this.label2.TabIndex = 36;
+            this.label2.Text = "Refresh";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(1085, 156);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(71, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 35;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.PictureBox1_Click);
+            // 
+            // dgvnishikiconsecsessionsrooms
+            // 
+            this.dgvnishikiconsecsessionsrooms.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvnishikiconsecsessionsrooms.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.MenuHighlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvnishikiconsecsessionsrooms.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgvnishikiconsecsessionsrooms.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvnishikiconsecsessionsrooms.Location = new System.Drawing.Point(32, 225);
+            this.dgvnishikiconsecsessionsrooms.Name = "dgvnishikiconsecsessionsrooms";
+            this.dgvnishikiconsecsessionsrooms.RowHeadersWidth = 51;
+            this.dgvnishikiconsecsessionsrooms.RowTemplate.Height = 24;
+            this.dgvnishikiconsecsessionsrooms.Size = new System.Drawing.Size(1124, 524);
+            this.dgvnishikiconsecsessionsrooms.TabIndex = 34;
+            // 
+            // btnnishikiaddlecturerrooms
+            // 
+            this.btnnishikiaddlecturerrooms.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnnishikiaddlecturerrooms.BackColor = System.Drawing.Color.LightBlue;
+            this.btnnishikiaddlecturerrooms.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnnishikiaddlecturerrooms.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnnishikiaddlecturerrooms.Location = new System.Drawing.Point(409, 111);
+            this.btnnishikiaddlecturerrooms.Name = "btnnishikiaddlecturerrooms";
+            this.btnnishikiaddlecturerrooms.Size = new System.Drawing.Size(398, 60);
+            this.btnnishikiaddlecturerrooms.TabIndex = 33;
+            this.btnnishikiaddlecturerrooms.Text = "Add room";
+            this.btnnishikiaddlecturerrooms.UseVisualStyleBackColor = false;
+            this.btnnishikiaddlecturerrooms.Click += new System.EventHandler(this.Btnnishikiaddlecturerrooms_Click);
+            // 
             // Section4_addRoomsForconsecutivesessions
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.ClientSize = new System.Drawing.Size(1191, 774);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.dgvnishikiconsecsessionsrooms);
+            this.Controls.Add(this.btnnishikiaddlecturerrooms);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.nishikistatimg);
@@ -83,6 +153,8 @@
             this.Name = "Section4_addRoomsForconsecutivesessions";
             this.Text = "Section4_addRoomsForconsecutivesessions";
             ((System.ComponentModel.ISupportInitialize)(this.nishikistatimg)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvnishikiconsecsessionsrooms)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -93,5 +165,9 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.PictureBox nishikistatimg;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.DataGridView dgvnishikiconsecsessionsrooms;
+        private System.Windows.Forms.Button btnnishikiaddlecturerrooms;
     }
 }
---
+++ Section4_roomconsecsession_popup.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace ABC_Institute___Timetable_Generator
+{
+    partial class Section4_roomconsecsession_popup
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Section4_roomconsecsession_popup));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.nihsikidgvconsecsesroomdisplay = new System.Windows.Forms.DataGridView();
+            this.nishikiconsecseslecroom = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.nishikicmbconsecsesroomname = new System.Windows.Forms.ComboBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnnishikiconsecsesaddrooms = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nihsikidgvconsecsesroomdisplay)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.AliceBlue;
+            this.panel1.Controls.Add(this.nihsikidgvconsecsesroomdisplay);
+            this.panel1.Controls.Add(this.nishikiconsecseslecroom);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.nishikicmbconsecsesroomname);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.btnnishikiconsecsesaddrooms);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1181, 655);
+            this.panel1.TabIndex = 0;
+            // 
+            // nihsikidgvconsecsesroomdisplay
+            // 
+            this.nihsikidgvconsecsesroomdisplay.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.nihsikidgvconsecsesroomdisplay.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.nihsikidgvconsecsesroomdisplay.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.nihsikidgvconsecsesroomdisplay.Location = new System.Drawing.Point(12, 76);
+            this.nihsikidgvconsecsesroomdisplay.Name = "nihsikidgvconsecsesroomdisplay";
+            this.nihsikidgvconsecsesroomdisplay.RowHeadersWidth = 51;
+            this.nihsikidgvconsecsesroomdisplay.RowTemplate.Height = 24;
+            this.nihsikidgvconsecsesroomdisplay.Size = new System.Drawing.Size(1157, 341);
+            this.nihsikidgvconsecsesroomdisplay.TabIndex = 59;
+            this.nihsikidgvconsecsesroomdisplay.DoubleClick += new System.EventHandler(this.Nihsikidgvconsecsesroomdisplay_DoubleClick);
+            // 
+            // nishikiconsecseslecroom
+            // 
+            this.nishikiconsecseslecroom.Location = new System.Drawing.Point(494, 449);
+            this.nishikiconsecseslecroom.Name = "nishikiconsecseslecroom";
+            this.nishikiconsecseslecroom.Size = new System.Drawing.Size(395, 22);
+            this.nishikiconsecseslecroom.TabIndex = 58;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(294, 442);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 29);
+            this.label3.TabIndex = 57;
+            this.label3.Text = "Lecturer";
+            // 
+            // nishikicmbconsecsesroomname
+            // 
+            this.nishikicmbconsecsesroomname.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.nishikicmbconsecsesroomname.FormattingEnabled = true;
+            this.nishikicmbconsecsesroomname.Location = new System.Drawing.Point(494, 504);
+            this.nishikicmbconsecsesroomname.Name = "nishikicmbconsecsesroomname";
+            this.nishikicmbconsecsesroomname.Size = new System.Drawing.Size(395, 24);
+            this.nishikicmbconsecsesroomname.TabIndex = 56;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(14, 6);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(77, 64);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 55;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(97, 22);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(651, 32);
+            this.label4.TabIndex = 54;
+            this.label4.Text = "Add room for a particular consecutive session here";
+            // 
+            // btnnishikiconsecsesaddrooms
+            // 
+            this.btnnishikiconsecsesaddrooms.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.btnnishikiconsecsesaddrooms.BackColor = System.Drawing.Color.Turquoise;
+            this.btnnishikiconsecsesaddrooms.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnnishikiconsecsesaddrooms.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnnishikiconsecsesaddrooms.Location = new System.Drawing.Point(450, 562);
+            this.btnnishikiconsecsesaddrooms.Name = "btnnishikiconsecsesaddrooms";
+            this.btnnishikiconsecsesaddrooms.Size = new System.Drawing.Size(271, 81);
+            this.btnnishikiconsecsesaddrooms.TabIndex = 53;
+            this.btnnishikiconsecsesaddrooms.Text = "ADD";
+            this.btnnishikiconsecsesaddrooms.UseVisualStyleBackColor = false;
+            this.btnnishikiconsecsesaddrooms.Click += new System.EventHandler(this.Btnnishikiconsecsesaddrooms_Click);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(294, 499);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(149, 29);
+            this.label2.TabIndex = 52;
+            this.label2.Text = "Room Name";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(1122, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(47, 42);
+            this.button1.TabIndex = 51;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Section4_roomconsecsession_popup
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1181, 655);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Section4_roomconsecsession_popup";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Section4_roomconsecsession_popup";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nihsikidgvconsecsesroomdisplay)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridView nihsikidgvconsecsesroomdisplay;
+        private System.Windows.Forms.TextBox nishikiconsecseslecroom;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox nishikicmbconsecsesroomname;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnnishikiconsecsesaddrooms;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
